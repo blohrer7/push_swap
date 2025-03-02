5bb3791151746c39f6be9ff21065c046525ad8a3
blohrer <you@example.com>
2025-03-02 23:45:57 +0100

    delete print_stack

--- src/main.c
+++ src/main.c
@@ -6,11 +6,32 @@
 /*   By: blohrer <blohrer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 09:33:41 by blohrer           #+#    #+#             */
-/*   Updated: 2025/02/28 10:41:00 by blohrer          ###   ########.fr       */
+/*   Updated: 2025/03/02 23:44:47 by blohrer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+int	main(int argc, char *argv[])
+{
+	t_node	*stack_a;
+	t_node	*stack_b;
+
+	stack_a = NULL;
+	stack_b = NULL;
+	if (argc > 1)
+	{
+		parse_arguments(argc, argv, &stack_a);
+		if (is_sorted(stack_a))
+		{
+			clear(&stack_a, &stack_b);
+			return (0);
+		}
+		sort_stack(&stack_a, &stack_b);
+	}
+	clear(&stack_a, &stack_b);
+	return (0);
+}
 
 // int	main(int argc, char *argv[])
 // {
@@ -36,24 +57,3 @@
 // 	clear(&stack_a, &stack_b);
 // 	return (0);
 // }
-
-int	main(int argc, char *argv[])
-{
-	t_node	*stack_a;
-	t_node	*stack_b;
-
-	stack_a = NULL;
-	stack_b = NULL;
-	if (argc > 1)
-	{
-		parse_arguments(argc, argv, &stack_a);
-		if (is_sorted(stack_a))
-		{
-			clear(&stack_a, &stack_b);
-			return (0);
-		}
-		sort_stack(&stack_a, &stack_b);
-	}
-	clear(&stack_a, &stack_b);
-	return (0);
-}
