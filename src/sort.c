c30c18ca4205f8d4d8d820464d1e836200d75071
blohrer <you@example.com>
2025-03-02 16:09:20 +0100

    add parse in sort

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: blohrer <blohrer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 08:29:04 by blohrer           #+#    #+#             */
-/*   Updated: 2025/03/02 14:42:49 by blohrer          ###   ########.fr       */
+/*   Updated: 2025/03/02 16:08:02 by blohrer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,4 +56,27 @@ void	three_sort(t_node **stack)
 		reverse_rotate_a(stack);
 	if ((*stack)->value > (*stack)->next->value)
 		swap_a(stack);
+}
+
+void	parse_arguments(int argc, char *argv[], t_node **stack)
+{
+	char	**args;
+	int		i;
+	int		j;
+
+	i = 1;
+	while (i < argc)
+	{
+		args = ft_split(argv[i], ' ');
+		if (!args)
+			return ;
+		j = 0;
+		while (args[j])
+		{
+			validate_and_add(stack, args[j]);
+			j++;
+		}
+		ft_free(args);
+		i++;
+	}
 }
